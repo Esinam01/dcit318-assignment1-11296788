64a1b29a139494b5884d0c63287e09a96054ac3e
Mary Esinam Ahiakedi <meahiakedzi@st.ug.edu.gh>
2025-08-03 03:38:30 -0700

    TicketPriceCalculator commit

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age;
+
+            // Validate input
+            if (int.TryParse(Console.ReadLine(), out age) && age >= 0)
+            {
+                if (age <= 12 || age >= 65)
+                    Console.WriteLine("Ticket Price: GHC7");
+                else
+                    Console.WriteLine("Ticket Price: GHC10");
+            }
+            else
+            {
+                Console.WriteLine("Invalid age entered.");
+            }
+
+            // Pause the console
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
